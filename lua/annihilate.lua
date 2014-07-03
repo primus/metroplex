662aa05eb90280e0da2ea6c7c2cd8cc22da9dd6d
3rd-Eden <info@3rd-Eden.com>
2014-07-03 16:09:25 +0200

    [fix] The supplied namespace is already suffixed with `:`

--- lua/annihilate.lua
+++ lua/annihilate.lua
@@ -8,14 +8,14 @@ local address = assert(KEYS[1], 'The server address is missing')
 -- Get all the sparks for our given address so we can nuke them from our "global"
 -- spark registry>
 --
-local sparks = redis.call('SMEMBERS', namespace ..':'.. address ..':sparks')
+local sparks = redis.call('SMEMBERS', namespace .. address ..':sparks')
 
 --
 -- Iterate over all the sparks in our collection and completely nuke every spark
 -- which is connected on the given server address as it's dead.
 --
 for i = 1, #sparks do
-  redis.call('HDEL', namespace ..':sparks', sparks[i])
+  redis.call('HDEL', namespace ..'sparks', sparks[i])
 end
 
 --
@@ -25,8 +25,8 @@ end
 -- 2. Our server in the servers list
 -- 3. The keep alive server update
 --
-redis.call('DEL', namespace ..':'.. address ..':sparks');
-redis.call('SREM', namespace ..':servers', address);
-redis.call('DEL', namespace ..':'.. address);
+redis.call('DEL', namespace .. address ..':sparks');
+redis.call('SREM', namespace ..'servers', address);
+redis.call('DEL', namespace .. address);
 
 return 1
